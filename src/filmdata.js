74abdab98f888443c573415f46984f513adc27e6
Theo Björkqvist <theobjorkqvist@gmail.com>
2025-09-25 11:20:47 +0200

    jobbar på json-filen för filmer

---
+++ src/filmdata.js
@@ -0,0 +1,37 @@
+const filmer = [
+    {
+        id: 1,
+        titel: "The Master",
+        year: 2012,
+        director: "Paul Thomas Anderson",
+        plot: "A troubled WWII veteran becomes entangled with a charismatic cult leader, exploring power, faith, and human connection.",
+    },
+    {
+        id: 2,
+        titel: "Through a Glass Darkly",
+        year: 1961,
+        director: "Ingmar Bergman",
+        plot: "A young woman’s struggle with schizophrenia during a family vacation exposes fragile bonds and hidden tensions.",
+    },
+    {
+        id: 3,
+        titel: "Laurence Anyways",
+        year: 2012,
+        director: "Xavier Dolan",
+        plot: "A transgender woman’s decade-long journey of self-discovery and love strains her most important relationship.",
+    },
+    {
+        id: 4,
+        title: "Chungking Express",
+        year: 1994,
+        director: "Wong Kar-Wai",
+        plot: "Two lonely policemen in Hong Kong cross paths with a mysterious smuggler and a quirky snack bar worker, finding fleeting connections in the city’s restless nights.",
+    },
+    {
+        id: 5,
+        title: "Punch-Drunk Love",
+        plot,
+        director: "Paul Thomas Anderson",
+
+    },
+];
