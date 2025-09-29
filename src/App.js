18408c4ba535396004c34a6f856a31ee37d6811f
Theo Björkqvist <theobjorkqvist@gmail.com>
2025-09-29 11:36:57 +0200

    testar lite varianter

--- src/App.js
+++ src/App.js
@@ -5,11 +5,19 @@ import filmdata from './filmdata.json'
 
 function App() {
 
+  const filmer = [];
+  filmdata.forEach(film => {
+    filmer.push(<div class="filmcontainer">
+      <h2 key={film.id}>{film.title}</h2>
+      <h3 key={film.id}>{film.year}</h3>
+      <p key={film.id}>{film.plot}</p>
+    </div>
+    )
 
-
+  });
   return (
 
-    
+    <div>{filmer}</div>
 
 
 
