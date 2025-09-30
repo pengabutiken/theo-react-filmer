dc82e11f3bb035cdc6a3863b9d3950aa9c62659d
Theo Björkqvist <theobjorkqvist@gmail.com>
2025-09-30 16:35:09 +0200

    leker lite med font

--- src/App.js
+++ src/App.js
@@ -8,9 +8,9 @@ function App() {
   const filmer = [];
   filmdata.forEach(film => {
     filmer.push(<div class="filmcontainer">
-      <h2 key={film.id}>{film.title}</h2>
+      <h2 class="titel" key={film.id}>{film.title}</h2>
       <h3 key={film.id}>{film.year}</h3>
-      <p key={film.id}>{film.plot}</p>
+      <p class="plot" key={film.id}>{film.plot}</p>
       <img key={film.id} src={film.poster}></img>
     </div>
     )
@@ -18,7 +18,7 @@ function App() {
   });
   return (
 
-    <div>{filmer}</div>
+    <div> {filmer}</ div>
 
 
 
