e601b0661fc4faf7cd395319ba922ee6ecaa1b5c
Theo Björkqvist <113356799+pengabutiken@users.noreply.github.com>
2025-10-02 18:05:03 +0200

    saker har tagits bort när jag starta på annan dator så fixar det

--- src/App.js
+++ src/App.js
@@ -9,9 +9,9 @@ function App() {
   filmdata.forEach(film => {
     filmer.push(<div class="filmcontainer">
       <h2 class="titel" key={film.id}>{film.title}</h2>
-      <h3 key={film.id}>{film.year}</h3>
+      <h3 class="year"key={film.id}>{film.year}</h3>
       <p class="plot" key={film.id}>{film.plot}</p>
-      <img key={film.id} src={film.poster}></img>
+      <img class="bild" alt='filmbild' key={film.id} src={film.poster}></img>
     </div>
     )
 
