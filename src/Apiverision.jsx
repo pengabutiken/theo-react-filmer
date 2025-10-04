cd28f8aa0ef31aa34d5199090c987930f456c62d
Theo Björkqvist <113356799+pengabutiken@users.noreply.github.com>
2025-10-04 20:28:00 +0200

    fixa till headern så den stämmer till andra sidan, ska nu börja med api logiken

--- src/Apiverision.jsx
+++ src/Apiverision.jsx
@@ -8,11 +8,11 @@ function Apiverision() {
       <header className=" top-0 bg-gradient-to-r from-gray-950 to-black shadow-2xl p-4 sm:p-6 mb-10 z-10 flex items-center justify-between border-b border-gray-700">
         <h1 className="text-xl sm:text-2xl font-black text-white drop-shadow-lg tracking-tight truncate font-prata">
           a database of Theo's favorite movies, stored in various ways{" "}
-          <span className=" text-green-400">json</span>
+          <span className=" text-purple-400">api</span>
         </h1>
         <div className="flex space-x-3 flex-shrink-0 justify-end">
-          <button className="px-3 py-1 text-sm sm:px-4 sm:py-2  font-prata rounded-lg bg-transparent   text-purple-400  transition shadow-md whitespace-nowrap hover:bg-white/10">
-            API
+          <button className="px-3 py-1 text-sm sm:px-4 sm:py-2  font-prata rounded-lg bg-transparent   text-green-400  transition shadow-md whitespace-nowrap hover:bg-white/10">
+            Json
           </button>
           <button className="px-3 py-1 text-sm sm:px-4 sm:py-2 rounded-lg bg-transparent b text-red-300 font-prata transition shadow-md whitespace-nowrap hidden sm:inline-block hover:bg-white/10">
             Database
