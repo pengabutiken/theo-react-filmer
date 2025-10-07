bd25d44dade9280d172eac4e5477fc2e2263caf4
Theo Björkqvist <113356799+pengabutiken@users.noreply.github.com>
2025-10-07 02:04:59 +0200

    lekt runt lite med api

--- src/Apiverision.jsx
+++ src/Apiverision.jsx
@@ -1,8 +1,41 @@
 import React, { useEffect, useState } from "react";
 import "./App.css";
 import { Link } from "react-router-dom";
+const options = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyMzJkYjdkMmQ5NjAzMGU4OGZlNDhjZjQ0OTIwODk1MyIsIm5iZiI6MTcxNDY1MTc3My45MzEsInN1YiI6IjY2MzM4MjdkYzYxNmFjMDEyODE5M2NmZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.jtLAJrH3afnqvkIvOadb9v7exk5p_DzcwwhtDR2zyA4'
+  }
+};
 
+const filmID=[
+    68722, //the master
+    11602,//såsom
+    110160,// laurnece
+    11104, // chungking
+    8051,// punch
+    27324,//pulse
+    3175,//barry
+    3121,//nashville
+    8927,//de ofrivilliga
+    240,//gudfadern
+]
+
+  
 function Apiverision() {
+    const [movie, setMovie]=useState(null);
+
+      useEffect(() => {
+    fetch('https://api.themoviedb.org/3/movie/8051', options)
+      .then(res => res.json())
+      .then(res => {
+        console.log(res); 
+        setMovie(res);    
+      })
+      .catch(err => console.error(err));
+  }, []);
+
   return (
     <div className="min-h-screen bg-gray-100 font-sans">
       <header className=" top-0 bg-gradient-to-r from-gray-950 to-black shadow-2xl p-4 sm:p-6 mb-10 z-10 flex items-center justify-between border-b border-gray-700">
@@ -22,6 +55,8 @@ function Apiverision() {
           </button>
         </div>
       </header>
+      {movie ? <h1>{movie.title}</h1> : <p>Laddar...</p>}
+      
     </div>
   );
 }
